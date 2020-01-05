b28210c119800ad99f4f6cfbb88e94d948f01261
Hi1talib1World <31773165+Hi1talib1World@users.noreply.github.com>
2020-01-05 23:18:12 +0100

    AppNavigation

---
+++ src/components/AppNavigation.vue
@@ -0,0 +1,20 @@
+<template>
+        <v-toolbar app color="brown darken-4" dark>
+            <v-toolbar-side-icon></v-toolbar-side-icon>
+            <v-toolbar-title>{{appTitle}}</v-toolbar-title>
+            <v-btn flat>Menu</v-btn>
+            <v-spacer></v-spacer>
+            <v-btn flat>SIGN IN</v-btn>
+            <v-btn color="brown lighten-3">JOIN</v-btn>
+        </v-toolbar>
+
+</template>
+
+<script>
+export default {
+    name: 'AppNavigation'
+};
+</script>
+
+<style scoped>
+</style>
