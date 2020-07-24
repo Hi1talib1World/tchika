c814de8bcbc23fc25efb6e9b617b80d2fe270c18
Hi1talib1World <hichamoutaleb7@gmail.com>
2020-07-24 21:24:13 +0100

    Update Sidebar.vue

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,82 +1,84 @@
 <template>
   <div class="sidebar-wrapper">
     <nav
-        :class="{sidebar: true, sidebarStatic, sidebarOpened}"
-        @mouseenter="sidebarMouseEnter"
-        @mouseleave="sidebarMouseLeave"
+      :class="{ sidebar: true, sidebarStatic, sidebarOpened }"
+      @mouseenter="sidebarMouseEnter"
+      @mouseleave="sidebarMouseLeave"
     >
       <header class="logo">
-        <router-link to="/app"><span class="primary-word">Sing</span> App</router-link>
+        <router-link to="/app"
+          ><span class="primary-word">Sing</span> App</router-link
+        >
       </header>
       <ul class="nav">
         <NavLink
-            :activeItem="activeItem"
-            header="Dashboard"
-            link="/app/dashboard"
-            iconName="flaticon-home"
-            index="dashboard"
-            isHeader
+          :activeItem="activeItem"
+          header="Dashboard"
+          link="/app/dashboard"
+          iconName="flaticon-home"
+          index="dashboard"
+          isHeader
         />
         <NavLink
-            :activeItem="activeItem"
-            header="Typography"
-            link="/app/typography"
-            iconName="flaticon-list"
-            index="typography"
-            isHeader
+          :activeItem="activeItem"
+          header="Typography"
+          link="/app/typography"
+          iconName="flaticon-list"
+          index="typography"
+          isHeader
         />
         <NavLink
-            :activeItem="activeItem"
-            header="Tables Basic"
-            link="/app/tables"
-            iconName="flaticon-equal-1"
-            index="tables"
-            isHeader
+          :activeItem="activeItem"
+          header="Tables Basic"
+          link="/app/tables"
+          iconName="flaticon-equal-1"
+          index="tables"
+          isHeader
         />
         <NavLink
-            :activeItem="activeItem"
-            header="Notifications"
-            link="/app/notifications"
-            iconName="flaticon-star"
-            index="notifications"
-            isHeader
+          :activeItem="activeItem"
+          header="Notifications"
+          link="/app/notifications"
+          iconName="flaticon-star"
+          index="notifications"
+          isHeader
         />
         <NavLink
-            :activeItem="activeItem"
-            header="Components"
-            link="/app/components"
-            iconName="flaticon-network"
-            index="components"
-            :childrenLinks="[
-              { header: 'Charts', link: '/app/components/charts' },
-              { header: 'Icons', link: '/app/components/icons' },
-              { header: 'Maps', link: '/app/components/maps' },
-            ]"
+          :activeItem="activeItem"
+          header="Components"
+          link="/app/components"
+          iconName="flaticon-network"
+          index="components"
+          :childrenLinks="[
+            { header: 'Charts', link: '/app/components/charts' },
+            { header: 'Icons', link: '/app/components/icons' },
+            { header: 'Maps', link: '/app/components/maps' }
+          ]"
         />
       </ul>
-      <p>
+      <p></p>
       <h5 class="navTitle">
         LABELS
         <a class="actionLink">
-          <i class="la la-plus float-right"/>
+          <i class="la la-plus float-right" />
         </a>
       </h5>
       <ul class="sidebarLabels">
         <li>
           <a href="#">
-            <i class="fa fa-circle text-danger mr-2"/>
+            <i class="fa fa-circle text-danger mr-2" />
             <span class="labelName">My Recent</span>
           </a>
         </li>
         <li>
           <a href="#">
-            <i class="fa fa-circle text-primary mr-2"/>
+            <i class="fa fa-circle text-primary mr-2" />
             <span class="labelName">Starred</span>
           </a>
         </li>
         <li>
           <a href="#">
-            <i class="fa fa-circle mr-2"/>
+            <i class="fa fa-circle mr-2" />
             <span class="labelName">Background</span>
           </a>
         </li>
@@ -86,15 +88,22 @@
       </h5>
       <div class="sidebarAlerts">
         <b-alert
-            v-for="alert in alerts"
-            :key="alert.id"
-            class="sidebarAlert" variant="transparent"
-            show dismissible
+          v-for="alert in alerts"
+          :key="alert.id"
+          class="sidebarAlert"
+          variant="transparent"
+          show
+          dismissible
         >
-          <span>{{alert.title}}</span><br/>
-          <b-progress class="sidebarProgress progress-xs mt-1"
-                      :variant="alert.color" :value="alert.value" :max="100"/>
-          <small>{{alert.footer}}</small>
+          <span>{{ alert.title }}</span
+          ><br />
+          <b-progress
+            class="sidebarProgress progress-xs mt-1"
+            :variant="alert.color"
+            :value="alert.value"
+            :max="100"
+          />
+          <small>{{ alert.footer }}</small>
         </b-alert>
       </div>
     </nav>
@@ -102,64 +111,64 @@
 </template>
 
 <script>
-import { mapState, mapActions } from 'vuex';
-import isScreen from '@/core/screenHelper';
-import NavLink from './NavLink/NavLink';
+import { mapState, mapActions } from "vuex";
+import isScreen from "@/core/screenHelper";
+import NavLink from "./NavLink/NavLink";
 export default {
-  name: 'Sidebar',
+  name: "Sidebar",
   components: { NavLink },
   data() {
     return {
       alerts: [
         {
           id: 0,
-          title: 'Sales Report',
+          title: "Sales Report",
           value: 15,
-          footer: 'Calculating x-axis bias... 65%',
-          color: 'danger',
+          footer: "Calculating x-axis bias... 65%",
+          color: "danger"
         },
         {
           id: 1,
-          title: 'Personal Responsibility',
+          title: "Personal Responsibility",
           value: 20,
-          footer: 'Provide required notes',
-          color: 'primary',
-        },
-      ],
+          footer: "Provide required notes",
+          color: "primary"
+        }
+      ]
     };
   },
   methods: {
-    ...mapActions('layout', ['changeSidebarActive', 'switchSidebar']),
+    ...mapActions("layout", ["changeSidebarActive", "switchSidebar"]),
     setActiveByRoute() {
-      const paths = this.$route.fullPath.split('/');
+      const paths = this.$route.fullPath.split("/");
       paths.pop();
-      this.changeSidebarActive(paths.join('/'));
+      this.changeSidebarActive(paths.join("/"));
     },
     sidebarMouseEnter() {
-      if (!this.sidebarStatic && (isScreen('lg') || isScreen('xl'))) {
+      if (!this.sidebarStatic && (isScreen("lg") || isScreen("xl"))) {
         this.switchSidebar(false);
         this.setActiveByRoute();
       }
     },
     sidebarMouseLeave() {
-      if (!this.sidebarStatic && (isScreen('lg') || isScreen('xl'))) {
+      if (!this.sidebarStatic && (isScreen("lg") || isScreen("xl"))) {
         this.switchSidebar(true);
         this.changeSidebarActive(null);
       }
-    },
+    }
   },
   created() {
     this.setActiveByRoute();
   },
   computed: {
-    ...mapState('layout', {
+    ...mapState("layout", {
       sidebarStatic: state => state.sidebarStatic,
       sidebarOpened: state => !state.sidebarClose,
-      activeItem: state => state.sidebarActiveElement,
-    }),
-  },
+      activeItem: state => state.sidebarActiveElement
+    })
+  }
 };
 </script>
 
 <!-- Sidebar styles should be scoped -->
-<style src="./Sidebar.scss" lang="scss" scoped/>
+<style src="./Sidebar.scss" lang="scss" scoped />
