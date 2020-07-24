5ea95e24f59edeab813fe3dff2a59ed0893083c3
Hi1talib1World <hichamoutaleb7@gmail.com>
2020-07-24 21:24:53 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,54 @@
 <template>
-    <v-app>
-        <app-navigation></app-navigation>
-          <Sidebar />
+  <v-app>
+    <app-navigation></app-navigation>
+    <Sidebar />
 
-        <v-content transition="slide-x-transition">
-            <router-view>
-            
-            </router-view>
-        </v-content>
-    </v-app>
+    <v-content transition="slide-x-transition">
+      <router-view> </router-view>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import AppNavigation from '@/components/AppNavigation';
-import HelloWorld from '@/components/HelloWorld';
-import Sidebar from '@/components/Sidebar';
+import AppNavigation from "@/components/AppNavigation";
+import Sidebar from "@/components/Sidebar";
 
 export default {
-    name: 'App',
-    components: {
-        AppNavigation
+  name: "App",
+  components: {
+    AppNavigation,
+    Sidebar
+  },
+  methods: {
+    handleWindowResize() {
+      const width = window.innerWidth;
+      if (width <= 768 && this.sidebarStatic) {
+        this.toggleSidebar();
+        this.changeSidebarActive(null);
+      }
     }
+  }
+
+    computed: {
+        
+    created() {
+        const staticSidebar = JSON.parse(localStorage.getItem('sidebarStatic'));
+        if (staticSidebar) {
+        this.$store.state.layout.sidebarStatic = true;
+        } else if (!this.sidebarClose) {
+        setTimeout(() => {
+            this.switchSidebar(true);
+            this.changeSidebarActive(null);
+        }, 2500);
+        }
+        this.handleWindowResize();
+        window.addEventListener('resize', this.handleWindowResize);
+    },
+    beforeDestroy() {
+        window.removeEventListener('resize', this.handleWindowResize);
+    }
+
 };
 </script>
 
-<style>
-</style>
+<style></style>
