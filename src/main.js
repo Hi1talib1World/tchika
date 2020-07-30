42123e4ddd2eab81bb6023add198ba35b31e37b6
Hi1talib1World <hichamoutaleb7@gmail.com>
2020-07-30 23:37:10 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -10,7 +10,15 @@ Vue.component('Widget', Widget);
 Vue.use(VueGoogleMaps, {
   load: {
     key: 'AIzaSyB7OXmzfQYua_1LEhRdqsoYzyJOPh9hGLg',  // must change to own key
+  },
 });
+Vue.component('apexchart', VueApexCharts);
+Vue.mixin(layoutMixin);
+Vue.use(Toasted, {duration: 10000});
+
+Vue.config.productionTip = false;
+
+
 new Vue({
   vuetify,
   render: h => h(App)
