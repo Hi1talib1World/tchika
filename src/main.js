49f440c495cb9def4bf83d1dd55cdb890bda5fc7
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2022-03-01 21:09:22 +0100

    delete files

--- src/main.js
+++ src/main.js
@@ -1,24 +1,7 @@
-import Vue from "vue";
-import App from "./App.vue";
-import vuetify from "./plugins/vuetify";
-
-Vue.config.productionTip = false;
-Vue.use(BootstrapVue);
-Vue.use(VueTouch);
-Vue.use(Trend);
-Vue.component("Widget", Widget);
-Vue.use(VueGoogleMaps, {
-  load: {
-    key: "AIzaSyB7OXmzfQYua_1LEhRdqsoYzyJOPh9hGLg" // must change to own key
-  }
-});
-Vue.component("apexchart", VueApexCharts);
-Vue.mixin(layoutMixin);
-Vue.use(Toasted, { duration: 10000 });
-
-Vue.config.productionTip = false;
+import Vue from 'vue'
+import App from './App.vue'
 
 new Vue({
-  vuetify,
+  el: '#app',
   render: h => h(App)
-}).$mount("#app");
+})
