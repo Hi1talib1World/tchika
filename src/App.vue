c2fd859b41e2f1756547799b82f677b58090454b
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2022-03-01 18:26:49 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,30 @@
 <template>
-  <v-app>
-    <app-navigation></app-navigation>
-    <Sidebar />
-
-    <v-content transition="slide-x-transition">
-      <router-view> </router-view>
-    </v-content>
-  </v-app>
+            <div>
+                <app-NavBar >
+                </app-NavBar>
+            </div>
 </template>
 
 <script>
-import AppNavigation from "@/components/AppNavigation";
-import Sidebar from "@/components/Sidebar";
-import "./Layout.scss";
-
-export default {
-  name: "App",
-  components: {
-    AppNavigation,
-    Sidebar
-  },
-  methods: {
-    handleWindowResize() {
-      const width = window.innerWidth;
-      if (width <= 768 && this.sidebarStatic) {
-        this.toggleSidebar();
-        this.changeSidebarActive(null);
-      }
+    import NavBar from './components/NavBar.vue'
+    export default {
+        components:{
+            appNavBar:NavBar
+        }
     }
-  },
-
-  computed: {
-    created() {
-      const staticSidebar = JSON.parse(localStorage.getItem("sidebarStatic"));
-      if (staticSidebar) {
-        this.$store.state.layout.sidebarStatic = true;
-      } else if (!this.sidebarClose) {
-        setTimeout(() => {
-          this.switchSidebar(true);
-          this.changeSidebarActive(null);
-        }, 2500);
-      }
-      this.handleWindowResize();
-      window.addEventListener("resize", this.handleWindowResize);
-    },
-    beforeDestroy() {
-      window.removeEventListener("resize", this.handleWindowResize);
-    }
-  }
-};
 </script>
 
-<style></style>
+<style >
+    body{
+        margin:0;
+        font-family: "Lato", sans-serif;
+        font-weight: 700;
+        line-height: 1.2;
+        box-sizing: border-box;
+    }
+    html{
+    font-size:62.5%;
+    }
+</style>
+<style >
+</style>
