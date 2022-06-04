ddd4c660e91d5a06078fe6b1e25119bf146a3979
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2022-06-04 03:50:25 +0000

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,43 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App.vue'
+import App from './App'
 
+// Import the Vue Route module for registering routes
+import router from './router'
+
+// Import the vue resource module for making HTTP request ( like POST / GET etc )
+import Resource from 'vue-resource'
+
+// for graphs
+import Trend from 'vuetrend'
+Vue.use(Trend)
+
+// charts
+import Chartkick from 'chartkick'
+import VueChartkick from 'vue-chartkick'
+import Chart from 'chart.js'
+Vue.use(VueChartkick, { Chartkick })
+
+// Registering view plugins
+Vue.use(Resource)
+
+// Import global Vue compoments
+import navBar from './components/Navbar'
+Vue.component('app-navbar', navBar)
+
+// Global filters
+// This pulls in some random number
+Vue.filter('randomNumber', function(value) {
+  return Math.floor(Math.random() * (value - 1 + 1)) + 1
+})
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
 new Vue({
   el: '#app',
-  render: h => h(App)
+  router,
+  components: { App },
+  template: '<App/>'
 })
