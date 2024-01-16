1d8a5c5bb0398dc7b743af7a2d9cd0c758bd8ae1
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2024-01-16 11:40:55 +0100

    Update Home.vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,63 +1,65 @@
 <template>
   <div>
     <section class="hero is-info is-fullheight">
-
       <div class="hero-body">
         <div class="container has-text-centered">
-
-          <figure >
-            <img v-bind:src="logo" width="50%" height="20%">
+          <figure>
+            <img v-bind:src="logo" width="50%" height="20%" />
           </figure>
 
           <div class="column is-6 is-offset-3">
             <h1 class="title white">
-                {{ title }}
+              {{ title }}
             </h1>
             <h2 class="subtitle">
-                {{ subtitle1 }}
-              <br/>
-                {{ subtitle2 }}
+              {{ subtitle1 }}
+              <br />
+              {{ subtitle2 }}
             </h2>
             <div class="is-centered">
-              <a v-bind:href="bulmaWebsite" class="button is-primary">Visit Bulma</a>
-              <a v-bind:href="vueWebsite" class="button is-danger">Visit VueJS</a>
-              <a v-bind:href="repoLink" class="button is-info">Visit Repository</a>
+              <a v-bind:href="bulmaWebsite" class="button is-primary"
+                >Visit Bulma</a
+              >
+              <a v-bind:href="vueWebsite" class="button is-danger"
+                >Visit VueJS</a
+              >
+              <a v-bind:href="repoLink" class="button is-info"
+                >Visit Repository</a
+              >
             </div>
           </div>
-
         </div>
       </div>
-
     </section>
   </div>
 </template>
 
 <script>
-  import logo from './../assets/logo-white.png';
-  export default {
-    data: function() {
-      return {
-        'logo': logo,
-        'title': 'Hello Stranger !!!!',
-        'subtitle1': 'A demo website based on frameworks Bulma (CSS) and Vue JS (JS)',
-        'subtitle2': 'Feel free to navigate around and checkout the page',
-        'bulmaWebsite': 'https://bulma.io',
-        'vueWebsite': 'https://vuejs.org/',
-        'repoLink': 'https://github.com/faisaltheparttimecoder/bulma-vuejs-demo-website'
-      }
-    }
+import logo from "./../assets/logo-white.png";
+export default {
+  data: function() {
+    return {
+      logo: logo,
+      title: "Hello Stranger !!!!",
+      subtitle1:
+        "A demo website based on frameworks Bulma (CSS) and Vue JS (JS)",
+      subtitle2: "Feel free to navigate around and checkout the page",
+      bulmaWebsite: "https://bulma.io",
+      vueWebsite: "https://vuejs.org/",
+      repoLink:
+        "https://github.com/faisaltheparttimecoder/bulma-vuejs-demo-website"
+    };
   }
+};
 </script>
 
 <style scoped>
-  .hero.is-info {
-    background: linear-gradient(
-      rgba(0, 0, 0, 0.5),
-      rgba(0, 0, 0, 0.5)
-    ), url('https://unsplash.it/1200/900?random') no-repeat center center fixed;
-    -webkit-background-size: cover;
-    -moz-background-size: cover;
-    -o-background-size: cover;
-    background-size: cover;
-  }
+.hero.is-info {
+  background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
+    url("https://unsplash.it/1200/900?random") no-repeat center center fixed;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+}
 </style>
