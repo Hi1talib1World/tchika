87cb99a78d5b9090ab278fbd8ae472eab5a027ab
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2024-01-16 11:40:27 +0100

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import routes from './routes';
+import Vue from "vue";
+import Router from "vue-router";
+import routes from "./routes";
 
-Vue.use(Router)
+Vue.use(Router);
 
 // Create a new route instance and disable
 // the navigation to index page using mode.
 const router = new Router({
-  mode: 'history',
-  routes,
+  mode: "history",
+  routes
 });
 
 export default router;
