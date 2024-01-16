8c03f851600bbba3cbd3b23e1aa7cfed9df46ad6
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2024-01-16 11:41:01 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,28 @@
 <template>
-            <div>
-                <app-NavBar >
-                </app-NavBar>
-            </div>
+  <div>
+    <app-NavBar> </app-NavBar>
+  </div>
 </template>
 
 <script>
-    import NavBar from './components/NavBar.vue'
-    export default {
-        components:{
-            appNavBar:NavBar
-        }
-    }
+import NavBar from "./components/NavBar.vue";
+export default {
+  components: {
+    appNavBar: NavBar
+  }
+};
 </script>
 
-<style >
-    body{
-        margin:0;
-        font-family: "Lato", sans-serif;
-        font-weight: 700;
-        line-height: 1.2;
-        box-sizing: border-box;
-    }
-    html{
-    font-size:62.5%;
-    }
+<style>
+body {
+  margin: 0;
+  font-family: "Lato", sans-serif;
+  font-weight: 700;
+  line-height: 1.2;
+  box-sizing: border-box;
+}
+html {
+  font-size: 62.5%;
+}
 </style>
-<style >
-</style>
+<style></style>
