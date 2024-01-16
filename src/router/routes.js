08076e704f3ad671b6332588067285d5fef8cfd0
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2024-01-16 11:40:20 +0100

    Update routes.js

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,37 +1,37 @@
-import home from '@/components/Home'
-import dashboard from '@/components/Dashboard'
-import blogs from '@/components/News'
-import showblog from '@/components/Media'
-import aboutus from '@/components/AboutUs'
-import contact from '@/components/Contact'
+import home from "@/components/Home";
+import dashboard from "@/components/Dashboard";
+import blogs from "@/components/News";
+import showblog from "@/components/Media";
+import aboutus from "@/components/AboutUs";
+import contact from "@/components/Contact";
 
 export default [
   {
-    path: '/',
+    path: "/",
     component: home
   },
   {
-    path: '/dashboard/:menu',
+    path: "/dashboard/:menu",
     component: dashboard
   },
   {
-    path: '/blogs',
+    path: "/blogs",
     component: blogs
   },
   {
-    path: '/blog/:id/:users',
+    path: "/blog/:id/:users",
     component: showblog
   },
   {
-    path: '/aboutus',
+    path: "/aboutus",
     component: aboutus
   },
   {
-    path: '/team',
+    path: "/team",
     component: team
   },
   {
-    path: '/contact',
+    path: "/contact",
     component: contact
   }
 ];
