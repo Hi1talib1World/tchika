efe05fb541c7a00678692491a3a8c5ca7729dae6
hi1talib1world <hichamoutaleb7@gmail.com>
2024-07-15 14:51:48 +0100

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,23 @@
+const path = require('path');
+
 module.exports = {
-  transpileDependencies: ["vuetify"]
+  transpileDependencies: ["vuetify"],
+  chainWebpack: config => {
+    config.module
+      .rule('js')
+      .test(/\.js$/)
+      .exclude
+      .add(filepath => /node_modules/.test(filepath) && !/node_modules[\\/]vuetify/.test(filepath))
+      .end()
+      .use('cache-loader')
+      .loader('cache-loader')
+      .options({
+        cacheDirectory: path.resolve(__dirname, 'node_modules/.cache/babel-loader'),
+        cacheIdentifier: 'a6d0e15c'
+      })
+      .end()
+      .use('babel-loader')
+      .loader('babel-loader')
+      .end();
+  }
 };
